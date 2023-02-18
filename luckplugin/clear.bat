1997a776d31e8a75e537b665d0e5cc073bf999de
ColdTrade <youtube.com/@coldtrade>
2023-02-18 09:41:11 -0300

    LuckListener

--- luckplugin/clear.bat
+++
@@ -1,4 +0,0 @@
-cd server
-rm -rf world world_nether world_the_end
-cd ..
-
--- luckplugin/src/main/java/luckplugin/listener/LuckListener.java
+++ luckplugin/src/main/java/luckplugin/listener/LuckListener.java
@@ -1,8 +1,11 @@
 package luckplugin.listener;
 
+import java.util.List;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.LootGenerateEvent;
+import org.bukkit.inventory.ItemStack;
 
 import luckplugin.LuckPlugin;
 
@@ -15,10 +18,38 @@ public class LuckListener implements Listener {
 		this.plugin.hashCode();
 	}
 
+	boolean executed = false;
+
 	@EventHandler
 	public void lootGenerate(LootGenerateEvent event) {
-		this.plugin.getLogger().info("Handlers" + event.getHandlers());
-		this.plugin.getLogger().info("Luck: " + event.getLootContext().getLuck());
+		List<ItemStack> loot = event.getLoot();
+		for (ItemStack itemStack : loot) {
+			plugin.getLogger().info("itemStack " + itemStack + " " + itemStack.getType() + " " + itemStack.getAmount());
+			itemStack.setAmount(itemStack.getAmount() * 7);
+		}
+//		ItemStack stack = new ItemStack(Material.DIAMOND, 64);
+//		loot.add(stack);
+//		plugin.getLogger().info("Entity: " + event.getEntity());
+//		plugin.getLogger().info("getInventoryHolder: " + event.getInventoryHolder());
+//		plugin.getLogger().info("getLootedEntity: " + event.getLootContext().getLootedEntity());
+//		if (!executed) {
+//			LootContext lootContext = event.getLootContext();
+//			LootContext.Builder builder = new LootContext.Builder(lootContext.getLocation());
+//			builder = builder.killer(lootContext.getKiller());
+//			builder = builder.lootedEntity(lootContext.getLootedEntity());
+//			builder = builder.lootingModifier(lootContext.getLootingModifier());
+//			float luck = lootContext.getLuck();
+//			if (luck < Float.MAX_VALUE) {
+//				luck = Float.MAX_VALUE;
+//			}
+//			builder = builder.luck(luck);
+//			LootContext newLootContext = builder.build();
+//			event.getLootTable().fillInventory(event.getInventoryHolder().getInventory(), new Random(), newLootContext);
+//			plugin.getLogger().info("Increasing luck by a LOT");
+//			executed = true;
+//		} else {
+//			executed = false;
+//		}
 	}
 
 }
